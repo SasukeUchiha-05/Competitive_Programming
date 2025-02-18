0bea93db986b6deb7d9868dbe5757621598c195d
Karthik MVS <vijaysai.uchiha@gmail.com>
2025-02-18 20:27:39 +0530

    Update ClosestTripletSum.java

--- Unit-1_part-2/ClosestTripletSum.java
+++ Unit-1_part-2/ClosestTripletSum.java
@@ -59,3 +59,90 @@ Sample Output-2:
 Explanation:
 ------------
 The sum 10 + 9 + 8 = 27, which is the closest sum to 30 (minimum absolute difference |30 - 27| = 3). */
+
+
+import java.util.*;
+
+public class ClosestTripletSum{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] A = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            A[i] = sc.nextInt();
+        }
+        
+        int m = sc.nextInt();
+        int[] B = new int[m];
+        for(int i=0;i<m;i++)
+        {
+            B[i] = sc.nextInt();
+        }
+        
+        int p = sc.nextInt();
+        int[] C = new int[p];
+        for(int i=0;i<m;i++)
+        {
+            C[i] = sc.nextInt();
+        }
+        
+        int target = sc.nextInt();
+        sc.close();
+        
+        Solution sol = new Solution();
+        
+        int[] res = sol.closestTriplet(n,A,m,B,p,C,target);
+        
+        System.out.println(res[0] +" " + res[1] +" " + res[2] +" ");
+    }
+}
+
+class Solution{
+    public int[] closestTriplet(int n , int[] A , int m , int[] B , int p , int[] C , int target)
+    {
+        int closestSum = Integer.MAX_VALUE;
+        
+        int[] result = new int[3];
+        
+        for(int i=0;i<n;i++)
+        {
+            int rem = target -A[i];
+            
+            int[] closestPair = findClosestPair(B,C,rem);
+            
+            int currentSum = A[i] + closestPair[0] + closestPair[1];
+            
+            if (Math.abs(target - currentSum) < Math.abs(target - closestSum)){
+                closestSum = currentSum;
+                result[0] = A[i];
+                result[1] = closestPair[0];
+                result[2] = closestPair[1];
+            }
+            
+        }
+        return result; 
+    }
+    private int[] findClosestPair(int[] B, int[] C, int target){
+        int left = 0, right = C.length - 1;
+        int closestSum = Integer.MAX_VALUE;
+        int[] closestPair = new int[2];
+
+        while (left < B.length && right >= 0) {
+            int sum = B[left] + C[right];
+            if (Math.abs(target - sum) < Math.abs(target - closestSum)){
+                closestSum = sum;
+                closestPair[0] = B[left];
+                closestPair[1] = C[right];
+            }
+            if (sum < target){
+                left++; 
+            } else{
+                right--;
+            }
+        }
+        return closestPair;
+    }
+}
