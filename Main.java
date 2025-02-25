fa5a2cceca6bcaa735459259c7686c3e22b2253e
Karthik MVS <vijaysai.uchiha@gmail.com>
2025-02-25 11:37:11 +0530

    yesterday's commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    // public static int highestAltitude(int n, int[] arr) {
+    //     int maxHeight = 0;
+
+    //     for(int i=0;i<n-1;i++)
+    //     {
+    //         int currHeight = arr[i] + arr[i+1];
+    //         maxHeight = Math.max(currHeight,maxHeight);
+    //     }
+    //     return maxHeight; 
+    // }
+
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+        // int n = scanner.nextInt();
+        // int[] arr = new int[n];
+        // for (int i = 0; i < n; i++) {
+        //     arr[i] = scanner.nextInt();
+        // }
+        // int result = highestAltitude(n, arr);
+        // System.out.println(result);
+
+        
+    }
+}
