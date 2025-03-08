290b36d2d70a3b4609b15daf5cbd3cd10ee28352
Karthik MVS <vijaysai.uchiha@gmail.com>
2025-03-08 21:04:04 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,37 +1,36 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.*;
-
-public class Main {
-    // public static int highestAltitude(int n, int[] arr) {
-    //     int maxHeight = 0;
-
-    //     for(int i=0;i<n-1;i++)
-    //     {
-    //         int currHeight = arr[i] + arr[i+1];
-    //         maxHeight = Math.max(currHeight,maxHeight);
-    //     }
-    //     return maxHeight; 
-    // }
-
-    public static void main(String[] args) {
-        // Scanner scanner = new Scanner(System.in);
-        // int n = scanner.nextInt();
-        // int[] arr = new int[n];
-        // for (int i = 0; i < n; i++) {
-        //     arr[i] = scanner.nextInt();
-        // }
-        // int result = highestAltitude(n, arr);
-        // System.out.println(result);
-        // Set<Integer> obj = new LinkedHashSet<>();
-        // obj.add(1);
-        // obj.add(23);
-        // obj.add(45);
-        // obj.add(6);
-
-        // String[] array = {"kmit","ngit"};
-        // List<String> 
-        // System.out.println(obj);
-        System.out.println(12>>1);
-
-        
-    }
+class Emp{
+    int empno =20;
 }
+
+class A{
+
+    static int num=10;
+ 
+   public static void main(String[] args){
+ 
+      new A().m1(num);
+ 
+      System.out.print(" " + num);
+ 
+   }
+ 
+  
+ 
+ void m1(int num) {
+ 
+    num++;
+ 
+    for(int num = 3; num < 6; num++)
+ 
+         System.out.print(" " + num);
+ 
+    }
+ 
+ }
